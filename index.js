936421118e6ca0a01cfe9802a209a5dc22565d3b
Cara O'Shea <oshea.cara@gmail.com>
2018-02-26 23:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name)
+{
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name)
+{
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name)
+{
+  kittens.shift(name);
+}
+
+function appendKitten(name)
+{
+  const moreKittens = [...kittens, name];
+  return moreKittens;
+}
+
+function prependKitten(name)
+{
+  const moreKittens = [name, ...kittens];
+  return moreKittens;
+}
+
+function removeLastKitten(name)
+{
+  
+}
