31cffff6593a020ac47f59273fff1cbdba5288ff
Cara O'Shea <oshea.cara@gmail.com>
2018-02-26 23:36:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ function prependKitten(name)
 
 function removeLastKitten(name)
 {
-  kittens.slice(0, kittens.length - 1);
-  return kittens;
+  const lessKittens = kittens.slice(0, kittens.length - 1);
+  return lessKittens;
 }
 
 function removeFirstKitten(name)
